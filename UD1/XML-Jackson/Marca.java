56598fb2f9e21e1f95a0c49d30416e94ed3b8c98
utadm2 <122609426+utadm2@users.noreply.github.com>
2023-08-01 18:17:29 +0200

    Add files via upload

---
+++ UD1/XML-Jackson/Marca.java
@@ -0,0 +1,17 @@
+package com.utad;
+
+import java.util.List;
+
+public class Marca {
+    private String nombre;
+    private List<String> modelos;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+    public List<String> getModelos() {
+        return modelos;
+    }
+
+}
---
+++ UD1/XML-Jackson/Marcas.java
@@ -0,0 +1,12 @@
+package com.utad;
+
+import java.util.List;
+
+public class Marcas {
+    private List<Marca> marcas;
+
+    public List<Marca> getMarcas() {
+        return marcas;
+    }
+
+}
