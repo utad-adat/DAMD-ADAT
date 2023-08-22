185322165b88460f2d4ac1bcdf3d696a456e7d19
utadm2 <122609426+utadm2@users.noreply.github.com>
2023-08-22 19:03:19 +0200

    Create HibernateUtil.java

---
+++ UD3/HibernateUtil.java
@@ -0,0 +1,26 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public static void closeSession(Session session) {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+}
